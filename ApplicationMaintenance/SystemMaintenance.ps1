22330c131efd2fdf04a3d842459abef26fcde7e0
jchong <jcjmemail@gmail.com>
2014-06-12 11:16:28 -0400

    Added logging
    logs output to a file in the same folder as the script with the date in
    the filename

--- ApplicationMaintenance/SystemMaintenance.ps1
+++ ApplicationMaintenance/SystemMaintenance.ps1
@@ -4,49 +4,60 @@
 #
 #################################################################################
 
+# Custom variables - can be modified to suit your specific needs
+$shutdownDelay = 20				# seconds between each shutdown
+$startupDelay = 30				# seconds between each startup
+$clusterName = "Cluster1"
+
+
 $ScriptName = $MyInvocation.MyCommand.Name
-$shutdownDelay = 20								# seconds between each shutdown
-$startupDelay = 30								# seconds between each startup
-$clusterName = Cluster1
+
+# Logging setup - creates logfile in same folder with current date in the filename
+$logDate = (Get-Date).tostring("yyyyMMdd-HHmm")
+$logFile = '.\report.'+$logDate+'.log'
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path $logFile
 
 
 if ( $args.Count -ne 1 ) {
-		write-host ""
-        write-host "Usage: $ScriptName [start|stop]"
-		write-host ""
+		Write-Host ""
+        Write-Host "Usage: $ScriptName [start|stop]"
+		Write-Host ""
 		}
     elseif ( $args -eq "stop" ) {
 	    $GuestVM = Get-Content .\shutdownOrder.txt
-		write-host ""
-		write-host "Number of servers to shutdown: " $GuestVM.count
+		Write-Host ""
+		Write-Host "Number of servers to shutdown: " $GuestVM.count
 		$maintenanceTime = $GuestVM.count * $shutdownDelay / 60
-		write-host "Estimated time required for shutdown: " $maintenanceTime minutes
+		Write-Host "Estimated time required for shutdown: " $maintenanceTime minutes
 		foreach ($vm in $GuestVM) {
-			write-host ""
-			write-host "Shutting down $vm..."
+			Write-Host "Shutting down $vm..."
 			Get-Cluster $clusterName | Get-VM $vm | Shutdown-VMGuest -Confirm:$false
-			write-host ""
+			Write-Host ""
 			sleep $shutdownDelay
 			}
 	}
     elseif ($args -eq "start" ) {
 	    $GuestVM = Get-Content .\startupOrder.txt
-		write-host ""
-		write-host "Number of servers to start: " $GuestVM.count
+		Write-Host ""
+		Write-Host "Number of servers to start: " $GuestVM.count
 		$maintenanceTime = $GuestVM.count * $startupDelay / 60
-		write-host "Estimated time required for startup: " $maintenanceTime minutes
+		Write-Host "Estimated time required for startup: " $maintenanceTime minutes
 			foreach ($vm in $GuestVM) {
-			write-host ""
-			write-host "Starting up $vm..."
+			Write-Host "Starting up $vm..."
 			Get-Cluster $clusterName | Get-VM $vm | Start-VM -Confirm:$false
-			write-host ""
+			Write-Host ""
 			sleep $startupDelay
 			}
 	}
     else {
-		write-host ""
-		write-host "Options are either start or stop"
-		write-host ""
-		write-host "Example: $ScriptName start"
-		write-host ""
+		Write-Host ""
+		Write-Host "Options are either start or stop"
+		Write-Host ""
+		Write-Host "Example: $ScriptName start"
+		Write-Host ""
     }
+
+Stop-Transcript
